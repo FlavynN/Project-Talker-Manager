aabd2d6b043485d0378ce20e41d0fb4a4948c3bc
flavynn <flavenhow18@gmail.com>
2023-02-07 05:14:12 +0100

    Update: Req 2 Done!

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -4,16 +4,37 @@ const fs = require('fs/promises');
 
 const talkerRoute = express.Router();
 const HTTP_OK_STATUS = 200;
+const errorMessage = {
+  message: 'Pessoa palestrante não encontrada',
+};
+
+const talkerRouteFile = async () => {
+  const talkerPath = path.resolve(__dirname, '../talker.json');
+  try {
+    const talker = await fs.readFile(talkerPath, 'utf8');
+    return JSON.parse(talker);
+  } catch (error) {
+    return null;
+  }
+};
 
 talkerRoute.get('/', async (req, res) => {
-  const talkerPath = path.resolve(__dirname, '../talker.json');
-  const talker = await fs.readFile(talkerPath, 'utf8');
-  const talkers = JSON.parse(talker);
-
+  const talker = await talkerRouteFile();
   try {
-    res.status(HTTP_OK_STATUS).json(talkers);
+    res.status(HTTP_OK_STATUS).json(talker);
   } catch (error) {
     res.status(404).json([]);
+  }
+});
+
+talkerRoute.get('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const talkers = await talkerRouteFile();
+  const talkerFind = talkers.find((t) => t.id === id);
+  if (talkerFind) {
+    res.status(HTTP_OK_STATUS).json(talkerFind);
+  } else {
+    res.status(404).json(errorMessage);
   }
 });
 
